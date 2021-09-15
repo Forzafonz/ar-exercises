3f52492889b196b85c183efdbe4b76a246c55cde
Anton <golubin1990@gmail.com>
2021-09-14 21:41:16 -0400

    Add validation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store, presence: true
+  validates :hourly_rate, numericality: {in: 1..200}
+
+  belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,2 +1,14 @@
 class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validate :store_must_have_at_least_one_category
+
+  has_many :employees
+
+  def store_must_have_at_least_one_category
+    if !mens_apparel && !womens_apparel
+      errors.add(:category_empty, "both categories cannot be empty")
+    end
+  end
+
 end
